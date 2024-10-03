ddb1721ba5959656cdeb046264fc39afd34cf423
Vignesh Shetty <vigy048@outlook.com>
2024-10-03 19:30:09 +0530

    made changes to conftest

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 from Utils import config as con
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def test_setup(request):
     driver = webdriver.Chrome()
     chrome_options = webdriver.ChromeOptions()
